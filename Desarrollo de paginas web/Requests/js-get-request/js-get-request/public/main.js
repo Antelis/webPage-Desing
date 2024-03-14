9f0344ea25d957a8d89340d8041798a6f35aed53
Andres Benjamin Antelis Moreno <bantelis10@gmail.com>
2024-03-14 10:29:08 -0600

    Promises, get & post requests

--- Desarrollo de paginas web/Requests/js-get-request/js-get-request/public/main.js
+++ Desarrollo de paginas web/Requests/js-get-request/js-get-request/public/main.js
@@ -1,26 +1,30 @@
-// Information to reach API
 const url='https://api.datamuse.com/words?sl=';
 
-// Selects page elements
 const inputField = document.querySelector('#input');
 const submit = document.querySelector('#submit');
 const responseField = document.querySelector('#responseField');
 
-// Asynchronous function
+//Asynchronous function
 const getSuggestions = () => {
-  const wordQuery=inputField.value;
-  const endPoint=url+wordQuery;
-  fetch(endPoint)
-  .then(response=>{
-    if(response.ok){
+  const wordQuery = inputField.value;
+  const endPoint = `${url}${wordQuery}`;
+
+  //fetch request with endpoint, or url string with expected url and string value
+  fetch(endPoint, { cache: 'no-cache' }).then(response => {
+    if (response.ok) {
       return response.json();
     }
-    throw new Error('Request failed ');
-  }, networkError =>console.log(networkError.message)
-  );
+    //Handle cases where its falsey, aka, when it failed to retrieve response
+    throw new Error('Request failed: ' + response.status);
+  }, networkError => {
+    //Handle network errors
+    console.log(networkError.message);
+  }).then(jsonResponse => {
+    // Handle successful response
+    renderRawResponse(jsonResponse); //JSON response
+  });
 }
-
-// Clears previous results and display results to webpage
+//Clears previous results and display results to webpage
 const displaySuggestions = (event) => {
   event.preventDefault();
   while(responseField.firstChild){
